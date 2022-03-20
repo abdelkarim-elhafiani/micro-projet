620abcd54d3959344b9083351840e835d178ab1b
abdelkarim elhafiani <abdelkarimelhafiani@gmail.com>
2022-03-20 16:18:28 +0100

    add model

--- src/main/java/models/Article.java
+++ src/main/java/models/Article.java
@@ -1,4 +1,4 @@
-package model;
+package models;
 
 public class Article {
 	private String codeArticle;
--- src/main/java/models/LignesCommande.java
+++ src/main/java/models/LignesCommande.java
@@ -1,4 +1,4 @@
-package model;
+package models;
 
 public class LignesCommande {
 	private String numCommande;
